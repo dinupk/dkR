11c8b2e4692c799f8ac085a2683db47a84bfed5b
Dinup Kulangara <dinupkulangara@gmail.com>
2020-08-09 09:56:11 +0530

    Added Factor Lessons

--- Matrix-Coursera.R
+++ Matrix-Coursera.R
@@ -1,3 +1,14 @@
-m1<-matrix(nrow=2,ncol=3)
+[#Matrices
+m1<-matrix(1:6,nrow=2,ncol=3,byrow=TRUE)
 m1
-att<-(attributes(m1)
+attributes(m1)
+      
+v1<-c(1:10)
+v1
+dim(v1)<-c(2,5)
+v1
+#Factors
+fact1<-factor(c("Male","Female","Male","Male","Female"))
+fact1
+tabfact1<-table(fact1)
+tabfact1["Male"]
