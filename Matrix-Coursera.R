b743846e6132e5eb9783fbc9cd68c4e82b1b7132
Dinup <dinupk@users.noreply.github.com>
2020-08-09 09:57:52 +0530

    Update Matrix-Coursera.R

--- Matrix-Coursera.R
+++ Matrix-Coursera.R
@@ -1,4 +1,4 @@
-[#Matrices
+#Matrices
 m1<-matrix(1:6,nrow=2,ncol=3,byrow=TRUE)
 m1
 attributes(m1)
@@ -7,7 +7,7 @@ v1<-c(1:10)
 v1
 dim(v1)<-c(2,5)
 v1
-#Factors
+#Factors Github
 fact1<-factor(c("Male","Female","Male","Male","Female"))
 fact1
 tabfact1<-table(fact1)
