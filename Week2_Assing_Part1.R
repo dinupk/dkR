4f2d57f5880ce00c6e83954cfb5b10c391140bb5
Dinup Kulangara <dinupkulangara@gmail.com>
2020-08-15 13:38:26 +0530

    Correcting errors.

--- Week2_Assing_Part1.R
+++ Week2_Assing_Part1.R
@@ -1,17 +1,14 @@
 pollutantmean<-function(directory,pollutant,id=1:332){
- print(directory)
  id<-sprintf("%03d",id)
- PollutantData=list()
+ PollutanttData<-list()
  for ( pollutIndex in seq_along(id)){
-  
- #     CurrRead <-read.csv(file=paste0(directory,"/",id[pollutIndex],".csv"),header = TRUE)
       CurrRead<-read.csv(file=paste0(directory,"/",id[pollutIndex],".csv"),header = TRUE)
-      PollutantData<-append(PollutantData,CurrRead)
+      PollutantData=rbind(PollutantData,CurrRead)
+
             }
-  
+  print(unique(PollutantData$ID))
   mean(get(pollutant,PollutantData),na.rm=TRUE)
-  
- }
+}
 
 pollutid<-c(2,40,124,325,155)
 data<-pollutantmean(getwd(),"sulfate",pollutid)
