0327cd64161ed065bc6e967a228120df269230ef
Dinup Kulangara <dinupkulangara@gmail.com>
2020-08-15 17:08:59 +0530

    complete.R verified

--- complete.R
+++ complete.R
@@ -3,12 +3,8 @@ complete<-function(directory,id=1:332){
   
    filelist<-list.files(directory)
    id<-sprintf("%03d",id)
-   print(id)
    for(fileIndex in seq_along(id)){
-    print(fileIndex) 
-    print(id[fileIndex])
     fname<-paste0(directory,"/",id[fileIndex],".csv")
-    print(fname)
     currRead<-read.csv(fname,header=TRUE)
     currRead<-na.omit(currRead)
     Idtag<-currRead[1,"ID"]
